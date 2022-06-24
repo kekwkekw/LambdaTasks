6e17b2aaf6a3c1e173a2b3ca3f24ab5c438a9223
Vintonyk Danylo <87479958+kekwkekw@users.noreply.github.com>
2022-06-24 14:40:45 +0300

    Add files via upload

---
+++ CLI_echo_bot/CLI_telegram_echo.js
@@ -0,0 +1,22 @@
+//node CLI_telegram_echo.js
+process.env.NTBA_FIX_319 = 1;
+process.env.NTBA_FIX_350 = 1;
+const TelegramBot = require('node-telegram-bot-api');
+const token = '5494843165:AAEcf23byKW_DuyblVI42cw5v49wYJh2k7s';
+const bot = new TelegramBot(token, {polling: true});
+const axios = require('axios').default;
+const fs = require("fs");
+
+bot.on('message', async (msg) => {
+    const chatId = await msg.chat.id;
+    const text = await msg.text;
+    if (text === 'photo'){
+        const res = await axios.get('https://picsum.photos/200/300', {
+            responseType: 'stream'})
+        bot.sendPhoto(chatId, res.data)
+    }
+    else{
+        bot.sendMessage(chatId, `Вы написали '${text}'`);
+    }
+});
+
