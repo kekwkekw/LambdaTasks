753de00d15255f2420d7c778a8d74139f175f4a7
kekwkekw <danulo2403@gmail.com>
2022-07-09 16:53:46 +0300

    hehe

--- Geoposition by IP/app.js
+++ Geoposition by IP/app.js
@@ -1,13 +1,16 @@
 // http://localhost:3000/
+const express = require('express');
+const findIp = require('./find_ip')
 
 const app = express();
 const port = 3000;
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get('/', async (req, res) => {
+    let result = await findIp.findIp(req.headers['x-forwarded-for'])
+    console.log('Returned response: ', result,'\n\n')
+    res.send(result);
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`http://localhost:${port}`);
 })
 
