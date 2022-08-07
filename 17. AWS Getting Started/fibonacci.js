4ba2d8a52f33816abde653584ddd12558a8d4d57
kekwkekw <danulo2403@gmail.com>
2022-08-07 10:34:53 +0300

    hehe

---
+++ 17. AWS Getting Started/fibonacci.js
@@ -0,0 +1,11 @@
+function fibonacci(n) {
+    if (n === 1) {
+        return [0, 1]
+    }
+    else {
+        let arr = fibonacci(n - 1)
+        arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+        return arr;
+    }
+}
+console.log(fibonacci(50))
